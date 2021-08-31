50d48ed5cf0cb9cccb98fb4bc069f28846a9bed6
testUser453 <71616101+testUser453@users.noreply.github.com>
2021-08-31 17:08:58 -0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,40 @@
+#On this file we will be learning about variables
+
+#Lets start
+
+#What is a variable? A variable is a letter, word, etc that you initiate to store some type of value like integers, strings, or floats
+
+#For starters there are different types of variables a list of them are integers, strings, floats, and boolean values
+
+
+#lets start with the integer variable
+#To start, we have to use a variable, in our example we will be using 'x' to hold the value of integer 56
+#To give the variable (x) the value of 56 we do this: x = 56
+#To do math operations (math operations will be covered in mathop.py) you will have to use the integer variable
+x = 56
+#now you can see that we have given 'x' the value of 56, to see if 'x' actually has the value of 56, we can print it. (You will learn about the print statement in print.py but just look for now)
+print(x)
+#if you copied and pasted this code or downloaded it to run it in your IDE or text editor, you will see that the integer 56 will be printed
+
+
+#Now that we have learned about the integer variable, lets learn about the string variable
+#The string variable is where you can store any character, but note that if you store a number, it will not have math operation functions or other integer functions
+#I will show you how to use the string variable using variable 'y' and will also print it right after assigning the string value of 'Hello World'
+y = 'Hello World'
+print(y)
+
+#We will now learn about the float variable
+#In float variables you can have decimal numbers, something that the integer variable doesn't posses
+#I will show you how to use the float variable using variable 'z' and will also print it right after assigning the float value of '3432.242'
+z = 3432.242
+print(z)
+
+
+
+#We will now learn about the boolean variable
+#In boolean variables you will be able to store the values of either True or False
+#I will show you how to use the boolean variable using variable 'a' and will also print it right after assigning the boolean value of 'True'
+a = True
+print(a)
+
+#You now have learned about the basic variables, look at the CourseGuide.txt to move on to the next topic
