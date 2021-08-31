d2ede0d284f02be75680d1dc87833d2a9a24bd9b
testUser453 <71616101+testUser453@users.noreply.github.com>
2021-08-31 17:20:15 -0400

    Delete input.py

--- input.py
+++
@@ -1,7 +0,0 @@
-feeling = input("input a feeling: ", )
-adjective = input("input an adjective that describes a car: ", )
-carbrand = input("input your favorite car brand: ", )
-name = input("what is your name?: ", )
-
-
-print(name, " was feeling ", feeling, " because his favorite car, a ", carbrand, "was stolen")
