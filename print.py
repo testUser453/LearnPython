5c3595bc1e1f59f1fda6bc6bbaa652f8a276c9ee
testUser453 <71616101+testUser453@users.noreply.github.com>
2021-08-31 17:29:44 -0400

    Add files via upload

---
+++ print.py
@@ -0,0 +1,24 @@
+#In this file, you will be learning about print statements
+
+#Lets start of by simply printing Hello World
+#To print a statement like Hello World, we use the print statement (keep in mind that you can use single or double quotes) --> print("This is where what you want to say goes")
+#Using the print statement, lets print 'Hello World'
+print("Hello World")
+
+#If you printed this statement, in your terminal console of your text editor, IDE or computer you will see the words 'Hello World'
+#In the example above you can see that we printed the string 'Hello World'
+
+#If you would like to print an integer you do this print(your integer value)
+#Lets do this by printing the integer 5
+print(5)
+
+#In your terminal you can see that we printed the value 5
+
+#Now lets print the value of a variable 'x' that equals the integer 10
+x = 10
+print(x)
+
+#Now, in your terminal you can see the number 10
+
+
+#That is the basics of the print statement you will learn more about different ways the print statement can be used in other topics like math operations (mathop.py) and etc.
